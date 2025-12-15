3590b94529b0a00f4d76ec981feb2ef9f717e366
adedola12 <dolapo836@gmail.com>
2025-12-15 18:11:32 +0100

    fix

--- client/src/components/HelpBot.jsx
+++ client/src/components/HelpBot.jsx
@@ -785,7 +785,7 @@ export default function HelpBot() {
             </button>
           </div>
 
-          <div className="px-3 pb-3">
+          {/* <div className="px-3 pb-3">
             <button
               onClick={() =>
                 window.open(
@@ -798,7 +798,7 @@ export default function HelpBot() {
             >
               Chat with Live Support on WhatsApp
             </button>
-          </div>
+          </div> */}
         </div>
       )}
     </>
